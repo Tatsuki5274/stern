f4decf7e469308f0ba2555bed79ff6a157f80165
MM <Ia>
2019-03-28 21:50:53 +0900

    Auto stash before merge of "master" and "origin/master"

--- Hide/NormalStar.cpp
+++ Hide/NormalStar.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿#include"NormalStar.h"
 #include"DxLib.h"
 #include<math.h>
@@ -46,3 +47,51 @@ void NormalStar::inhale(int, int, float)
 	velocityX += (gravity_point.x - point.x) * 5;
 	velocityY += (gravity_point.x - point.x) * 5;*/
 }
+=======
+﻿#include"NormalStar.h"
+#include"DxLib.h"
+#include<math.h>
+#include"CoreTask.h"
+
+
+//--------------------------------
+//•’Ê‚Ì¯
+//--------------------------------
+
+NormalStar::NormalStar(Point point_, PhysicState physic_state_, StarState star_state) : Star(point_, physic_state_, star_state)
+{
+	velocityX = 3;
+	velocityY = 3;
+	init_render("star");
+}
+
+void NormalStar::update()
+{
+	DrawFormatString(300, 0, GetColor(255, 0, 0), "%d", angle);
+	if (!contact) {
+		point.x += (int)(-sin(angle) * velocityX);
+		point.y += (int)(cos(angle) * velocityY);
+		if (ct->gts->map->get_bottom(point) ||
+			ct->gts->map->get_top(point) ||
+			ct->gts->map->get_left(point) ||
+			ct->gts->map->get_right(point)) {
+			contact = true;
+		}
+	}
+	else {
+		this->exercise();
+	}
+	if (attack()) {
+	}
+
+	draw(true);
+	if (damage(1)) {
+		//ct->gts->normalstar->destroy();
+	}
+}
+
+void NormalStar::inhale(int, int, float)
+{
+}
+
+>>>>>>> Stashed changes
