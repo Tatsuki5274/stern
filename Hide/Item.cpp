91958a15d093b8202562a3ebb36b31565c01eb5f
Tatsuki Watanabe <42799529+Tatsuki5274@users.noreply.github.com>
2019-04-17 08:59:17 +0900

    条件文を変更

--- Hide/Item.cpp
+++ Hide/Item.cpp
@@ -19,9 +19,9 @@ void Item::update()
 bool Item::check_hit(Point pl)
 {
 	bool check = false;
-	if (point.x < pl.x + pl.w ||
-		point.x + point.w > pl.x ||
-		point.y < pl.y + pl.h ||
+	if (point.x < pl.x + pl.w &&
+		point.x + point.w > pl.x &&
+		point.y < pl.y + pl.h &&
 		point.y + point.h > pl.y) {
 		check = true;
 	}
