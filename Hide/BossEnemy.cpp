324f5b90894b79f1b715d8b747bd456a94553793
tonegawa <1>
2019-04-18 19:25:45 +0900

    ボスを画面に出せるようにした
    enemy等の挙動がおかしいのでちゃんと行動ができているか分からない

--- Hide/BossEnemy.cpp
+++ Hide/BossEnemy.cpp
@@ -7,8 +7,9 @@
 Boss::Boss(Point point_, PhysicState physic_state_, EnemyState enemy_state_) : Enemy(point_, physic_state_, enemy_state_)
 {
 	bossstate = BossState::walk;
+	anglestate = AngleState::right;
 	movecnt = 0;
-	init_render("boss");
+	shape->set("boss");
 }
 
 void Boss::move()
