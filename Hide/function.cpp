f5500c2678b8ef328d8ab46ac1aea2ccf7778796
iipyon <kyota99930@yahoo.co.jp>
2019-04-24 19:07:00 +0900

    function追加中

---
+++ Hide/function.cpp
@@ -0,0 +1,18 @@
+#include"Point.h"
+bool CheckHit(Point attack_, Point defense_) {
+	if (attack_.x >= defense_.x + defense_.w
+		&&defense_.x >= attack_.x + attack_.w
+		&&attack_.y >= defense_.y + defense_.h
+		&&defense_.y >= attack_.y + attack_.h){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool Attack(Point attack_, Point defence_,int damege) {
+	if (CheckHit(attack_, defence_)) {
+
+	}
+}
