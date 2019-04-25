ea347344aabdba93e5132f1e5af1e5223983924b
MM <Ia>
2019-04-25 18:27:14 +0900

    private化

--- Hide/PauseTaskSelecter.h
+++ Hide/PauseTaskSelecter.h
@@ -11,11 +11,11 @@ private:
 	int x;
 	int y;
 	int graph;
+	void draw();
+	void switch_button();
+	void move(int, int, int);
 public:
 	PauseButton button;
 	PauseSelecter();
 	void update(int, int, int);
-	void draw();
-	void switch_button();
-	void move(int, int,int);
 };
