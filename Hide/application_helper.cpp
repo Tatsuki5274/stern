519a5e537a054cbc8635b19ff03ce4c3ad0a2526
Tatsuki <contactme0522@gmail.com>
2019-05-03 18:51:32 +0900

    構造変更と不要なファイル削除

--- Hide/application_helper.cpp
+++ Hide/application_helper.cpp
@@ -1,12 +1,17 @@
-﻿#include"Function.h"
+﻿#pragma once
+//アプリケーション全体で利用する機能を記述するファイル
+/*
+#include "application_helper.h"
 bool CheckHit(Point attack_, Point defense_) {
 	if (attack_.x <= defense_.x + defense_.w
 		&&defense_.x <= attack_.x + attack_.w
 		&&attack_.y <= defense_.y + defense_.h
-		&&defense_.y <= attack_.y + attack_.h){
+		&&defense_.y <= attack_.y + attack_.h) {
 		return true;
 	}
 	else {
 		return false;
 	}
 }
+
+*/
