763a8f1a72c557b9e78bc6c7a27153c274fbea23
MR <Ia>
2019-06-13 18:04:29 +0900

    ビルドが通るようにした謎エラーを解消

--- Hide/Player.cpp
+++ Hide/Player.cpp
@@ -1,4 +1,4 @@
-#include"GameTaskController.h"
+﻿#include"GameTaskController.h"
 #include "Player.h"
 #include"Point.h"
 #include"CoreTask.h"
@@ -21,8 +21,8 @@ Player::PlayerInterface::PlayerInterface()
 void Player::PlayerInterface::draw()
 {
 	//残機
-	DrawGraph(System::width-100, 0, lifegraph, FALSE);
-	DrawFormatString(System::width - 60, 0, GetColor(255, 255, 255), " × %d",life);
+	DrawGraph(System::width - 100, 0, lifegraph, FALSE);
+	DrawFormatString(System::width - 60, 0, GetColor(255, 255, 255), " × %d", life);
 	//HP
 	for (int i = 0; i < 3; ++i) {
 		DrawGraph(40 * i, 0, hpfreamgraph, FALSE);
@@ -32,7 +32,7 @@ void Player::PlayerInterface::draw()
 	}
 }
 
-void Player::PlayerInterface::update(int hp_,int life_)
+void Player::PlayerInterface::update(int hp_, int life_)
 {
 	hp = hp_;
 	life = life_;
@@ -46,7 +46,7 @@ Player::StarManager::StarManager()
 
 void Player::StarManager::draw(double st, int x)
 {
-	DrawRotaGraph2(x - Map::get_camera().x+ ct->gts->player->get_point().w / 2, 0, 15, 0, 1, st, graph, FALSE);//Xにプレーヤー.wの半分だけついか
+	DrawRotaGraph2(x - Map::get_camera().x + ct->gts->player->get_point().w / 2, 0, 15, 0, 1, st, graph, FALSE);//Xにプレーヤー.wの半分だけついか
 }
 
 void Player::StarManager::update(double ang, int x_)
@@ -113,7 +113,7 @@ void Player::StarManager::update(double ang, int x_)
 	}
 }
 
-Player::Player(Point point_, PhysicState physic_state_):BasicObject(point)
+Player::Player(Point point_, PhysicState physic_state_) :BasicObject(point)
 {
 	life = PLAYER_MAX_LIFE;
 	hp = PLAYER_MAX_HP;
@@ -121,10 +121,8 @@ Player::Player(Point point_, PhysicState physic_state_):BasicObject(point)
 	invincible = 0;
 	jumpCnt = 0;
 	interval = 0;
-	//graph = LoadGraph("img/player.png");
 	starmanager = std::make_unique<StarManager>();
 	playerinterface = std::make_unique<PlayerInterface>();
-
 }
 void Player::spawn(int x_, int y_, int w_, int h_)
 {
@@ -133,7 +131,7 @@ void Player::spawn(int x_, int y_, int w_, int h_)
 void Player::init()
 {
 	point = p_point;
-	shape->set("player");//resource.jsonのnameが"player"のものをセットする
+	shape->set("player_idol_Left");//resource.jsonのnameが"player"のものをセットする
 }
 
 bool Player::recover()
@@ -160,7 +158,7 @@ void Player::update()
 	}
 	//---------------------------------------
 	starmanager->update(angle, point.x);
-	playerinterface->update(hp,life);
+	playerinterface->update(hp, life);
 	if (invincible % 4 <= 2) {
 		shape->draw(point);
 	}
@@ -193,9 +191,8 @@ void Player::move()
 {
 	//左右移動
 	if (Keyboard::key_press(KEY_INPUT_LEFT)) {
-		if (Keyboard::key_press(KEY_INPUT_C)/* && velocityX <= -6*/) { //仮のダッシュ処理
-			/*velocityX--;*/
-			point.x+= physicshape->Movement_X(point, -PLAYER_MAX_SPEED);
+		if (Keyboard::key_press(KEY_INPUT_C)) { //仮のダッシュ処理
+			point.x += physicshape->Movement_X(point, -PLAYER_MAX_SPEED);
 		}
 		else {
 			point.x += physicshape->Movement_X(point, -PLAYER_SPEED);
@@ -203,9 +200,6 @@ void Player::move()
 	}
 	if (Keyboard::key_press(KEY_INPUT_RIGHT)) {
 		if (Keyboard::key_press(KEY_INPUT_C)) {  //仮のダッシュの処理
-			/*if (velocityX <= +6) {
-				velocityX++;
-			}*/
 			point.x += physicshape->Movement_X(point, PLAYER_MAX_SPEED);
 		}
 		else {
@@ -213,22 +207,18 @@ void Player::move()
 		}
 	}
 	//ジャンプ
-	if (point.y==preY) {
+	if (point.y == preY) {
 		if (Keyboard::key_press(KEY_INPUT_X)) {
-
-
 			if (Keyboard::key_down(KEY_INPUT_X)) {
 				jumpCnt = PLAYER_MAX_JUMP;
 			}
 			if (jumpCnt > 0) {
-				
 				point.y += physicshape->Movement_Y(point, -jumpCnt - 8);//jumpCntを設けないと空中浮遊する
 				Point extendpoint = point;
 				extendpoint.y--;
 				if (ct->gts->map->get_top(extendpoint)) {
 					jumpCnt = 0;
 				}
-
 			}
 		}
 	}
@@ -279,5 +269,4 @@ void Player::set_life(int life) {
 }
 int Player::get_life() {
 	return this->life;
-
 }
